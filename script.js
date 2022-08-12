16aa2b7b86fd9096dfc9561bf56ac359867f2d45
scottbishop <sa_bishop@comcast.net>
2022-08-12 09:27:42 -0400

    Switched to switch statements :)

--- script.js
+++ script.js
@@ -8,44 +8,56 @@ function handleConversion(event) {
 
   const valueInput = document.querySelector("#selectorInput").value;
   const valueOutput = document.querySelector("#selector").value;
-  const tempF = parseInt(document.getElementById("tempF").value);
-  let toCelsius = 0;
-
-  if (valueInput === "fahrenheit") {
-    toCelsius = +(((tempF - 32) * 5) / 9).toFixed(2);
-  } else if (valueInput === "celsius") {
-    toCelsius = tempF;
-  } else if (valueInput === "kelvin") {
-    toCelsius = +(tempF - 273.15).toFixed(2);
-  } else if (valueInput === "rankine") {
-    toCelsius = +(((tempF - 491.67) * 5) / 9).toFixed(2);
-  } else if (valueInput === "romer") {
-    toCelsius = +(((tempF - 7.5) * 40) / 21).toFixed(2);
-  } else if (valueInput === "delisle") {
-    toCelsius = +(100 - (tempF * 2) / 3).toFixed(2);
-  } else {
-    toCelsius = +((tempF * 5) / 4).toFixed(2);
+  const tempF = parseFloat(document.getElementById("tempF").value);
+  let toCelsius;
+  //converts all inputed temperatures to celsius
+  switch (valueInput) {
+    case "fahrenheit":
+      toCelsius = +(((tempF - 32) * 5) / 9).toFixed(2);
+      break;
+    case "celsius":
+      toCelsius = tempF;
+      break;
+    case "kelvin":
+      toCelsius = +(tempF - 273.15).toFixed(2);
+      break;
+    case "rankine":
+      toCelsius = +(((tempF - 491.67) * 5) / 9).toFixed(2);
+      break;
+    case "romer":
+      toCelsius = +(((tempF - 7.5) * 40) / 21).toFixed(2);
+      break;
+    case "delisle":
+      toCelsius = +(100 - (tempF * 2) / 3).toFixed(2);
+      break;
+    default:
+      toCelsius = +((tempF * 5) / 4).toFixed(2);
   }
+  //converts celsius to desired temperture
+  let convertedText;
+  switch (valueOutput) {
+    case "fahrenheit":
+      convertedText = `${+((toCelsius * 9) / 5 + 32).toFixed(2)}° F`;
+      break;
+    case "celsius":
+      convertedText = `${toCelsius}° C`;
+      break;
+    case "kelvin":
+      convertedText = `${+(toCelsius + 273.15).toFixed(2)}° K`;
+      break;
+    case "rankine":
+      convertedText = `${+(((toCelsius + 273.15) * 9) / 5).toFixed(2)}° Ra`;
+      break;
+    case "romer":
+      convertedText = `${+((toCelsius * 21) / 40 + 7.5).toFixed(2)}° Rø`;
+      break;
+    case "delisle":
+      convertedText = `${+(((100 - toCelsius) * 3) / 2).toFixed(2)}° D`;
+      break;
+    default:
+      convertedText = `${+((toCelsius * 4) / 5).toFixed(2)}° Ré`;
+  }
+  resultsSpan.innerHTML = convertedText;
 
-  valueOutput === "fahrenheit"
-    ? (resultsSpan.innerHTML = `${+((toCelsius * 9) / 5 + 32).toFixed(2)}° F`)
-    : valueOutput === "celsius"
-    ? (resultsSpan.innerHTML = `${toCelsius}° C`)
-    : valueOutput === "kelvin"
-    ? (resultsSpan.innerHTML = `${+(toCelsius + 273.15).toFixed(2)}° K`)
-    : valueOutput === "rankine"
-    ? (resultsSpan.innerHTML = `${+(((toCelsius + 273.15) * 9) / 5).toFixed(
-        2
-      )}° Ra`)
-    : valueOutput === "romer"
-    ? (resultsSpan.innerHTML = `${+((toCelsius * 21) / 40 + 7.5).toFixed(
-        2
-      )}° Rø`)
-    : valueOutput === "delisle"
-    ? (resultsSpan.innerHTML = `${+(((100 - toCelsius) * 3) / 2).toFixed(
-        2
-      )}° D`)
-    : (resultsSpan.innerHTML = `${+((toCelsius * 4) / 5).toFixed(2)}° Ré`);
+  convertButton.onclick = handleConversion;
 }
-
-convertButton.onclick = handleConversion;
